635cb87599747f9dbbdb820b4dbb8c596b287961
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-09-19 12:00:35 +0700

    first commit

---
+++ app/app.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from flask import Flask
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'haloo sobat jagoan'
