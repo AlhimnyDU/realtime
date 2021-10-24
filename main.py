cf5edc2185df2fc9286e2db1ef150e1f4f283f16
AlhimnyDU <dwinata1@mhs.itenas.ac.id>
2021-10-24 16:38:25 +0700

    kuesioner

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ UPLOAD_FOLDER = './static/gambar/'
 UPLOAD_FOLDER = './static/input/'
 app.secret_key = 'any random string'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'tracerstudy_studypk'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'T42ngk4l4n'
-app.config['MYSQL_DATABASE_DB'] = 'tracerstudy_ts_bka'
+# app.config['MYSQL_DATABASE_USER'] = 'tracerstudy_studypk'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'T42ngk4l4n'
+# app.config['MYSQL_DATABASE_DB'] = 'tracerstudy_ts_bka'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'ts_bka'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 mysql = MySQL(app)
 con = mysql.connect()
@@ -43,7 +46,8 @@ def index():
 def piechart(id_fitur):
     atribut = []
     explode = []
-    print(id_fitur)
+    index = []
+    # print(id_fitur)
     date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     cur = mysql.get_db().cursor()
     cur.execute("SELECT * FROM fitur WHERE id_fitur=%s", (id_fitur))
@@ -57,14 +61,21 @@ def piechart(id_fitur):
     cur.execute("SELECT * FROM variabel WHERE id_fitur=%s", (id_fitur))
 
     variabel = cur.fetchall()
+    i = 1
     for a in variabel:
         atribut.append(a[1])
         explode.append(0.05)
+        index.append(i)
+        i+=1
+
+    fix_atribut = atribut
 
     excel="../public_html/cdc/excel/kuesioner/"+kuesioner[4]
-    print(excel)
+    # excel="D:/Proyek/xampp/ts_bka/excel/kuesioner/"+kuesioner[4]
+
+    # print(excel)
     df = pd.read_excel(excel)
-    print(df)
+    # print(df)
     if (df.empty):
         df_olah = df
         jml = 0
@@ -74,35 +85,39 @@ def piechart(id_fitur):
         jml = df_olah['NRP'].sum()
         df_olah = df_olah.reset_index()
 
-
-
+    print(index)
+    print(df_olah[fitur[1]].values)
     if (df_olah[fitur[1]].count() != 0):
         if (df_olah[fitur[1]].count() < len(atribut)):
             cek = array_diff(index,df_olah[fitur[1]].values)
             print(cek)
             if cek is not None:
                 for j in cek:
-                    print(j)
-                    atribut.pop(j)
-                    explode.pop(j)
+                    # print(j)
+                    print(atribut[j-1])
+                    fix_atribut.remove(atribut[j-1])
+                    explode.pop(0)
     else:
         new_row = {fitur[1]: "NULL", 'NRP': 0}
         df_olah = df_olah.append(new_row, ignore_index=True)
-        atribut = ['Kosong']
+        fix_atribut = ['Kosong']
         explode = [0]
 
-    print(df_olah)
+    print(fix_atribut)
     print(len(atribut))
-    atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in atribut]
+    fix_atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in fix_atribut]
 
     # code here
     plt.figure(figsize=(12, 6))
     plt.title("Persentase Mahasiswa "+(fitur[2]), pad=20)
-    plt.pie(df_olah['NRP'], labels=atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2, explode=explode)
+    plt.pie(df_olah['NRP'], labels=fix_atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2, explode=explode)
     nama_file = secrets.token_hex(nbytes=16)+".png"
     plt.savefig("../public_html/cdc/data_olah/"+nama_file)
+    # plt.savefig("D:/Proyek/xampp/ts_bka/data_olah/"+nama_file)
     if(fitur[3]!=None):
         rmv='../public_html/cdc/data_olah/'+fitur[3]
+        # rmv='D:/Proyek/xampp/ts_bka/data_olah/'+fitur[3]
+
         os.remove(rmv)
 
     cur = con.cursor()
@@ -112,12 +127,14 @@ def piechart(id_fitur):
     con.commit()
 
     return redirect("https://tracerstudy.itenas.ac.id/cdc/admin/olah_fitur/"+id_fitur+"/"+str(jml))
+    # return redirect("http://localhost/ts_bka/admin/olah_fitur/"+id_fitur+"/"+str(jml))
 
 @app.route('/piechart_non/<id_fitur>', methods=['POST', 'GET'])
 def piechart_non(id_fitur):
     atribut = []
     explode = []
-    print(id_fitur)
+    index = []
+    # print(id_fitur)
     date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     cur = mysql.get_db().cursor()
     cur.execute("SELECT * FROM fitur WHERE id_fitur=%s", (id_fitur))
@@ -131,14 +148,19 @@ def piechart_non(id_fitur):
     cur.execute("SELECT * FROM variabel WHERE id_fitur=%s", (id_fitur))
 
     variabel = cur.fetchall()
+    i = 1
     for a in variabel:
         atribut.append(a[1])
         explode.append(0.05)
+        index.append(i)
+        i += 1
+
+    fix_atribut = atribut
 
     excel = "../public_html/cdc/excel/kuesioner/" + kuesioner[4]
-    print(excel)
+    # print(excel)
     df = pd.read_excel(excel)
-    print(df)
+    # print(df)
     if (df.empty):
         df_olah = df
         jml = 0
@@ -148,29 +170,32 @@ def piechart_non(id_fitur):
         jml = df_olah['NRP'].sum()
         df_olah = df_olah.reset_index()
 
+    print(index)
+    print(df_olah[fitur[1]].values)
     if (df_olah[fitur[1]].count() != 0):
         if (df_olah[fitur[1]].count() < len(atribut)):
             cek = array_diff(index, df_olah[fitur[1]].values)
             print(cek)
             if cek is not None:
                 for j in cek:
-                    print(j)
-                    atribut.pop(j)
-                    explode.pop(j)
+                    # print(j)
+                    print(atribut[j - 1])
+                    fix_atribut.remove(atribut[j - 1])
+                    explode.pop(0)
     else:
         new_row = {fitur[1]: "NULL", 'NRP': 0}
         df_olah = df_olah.append(new_row, ignore_index=True)
-        atribut = ['Kosong']
+        fix_atribut = ['Kosong']
         explode = [0]
 
-    print(df_olah)
+    print(fix_atribut)
     print(len(atribut))
-    atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in atribut]
+    fix_atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in fix_atribut]
 
     # code here
     plt.figure(figsize=(12, 6))
     plt.title("Persentase Mahasiswa " + (fitur[2]), pad=20)
-    plt.pie(df_olah['NRP'], labels=atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2, explode=explode)
+    plt.pie(df_olah['NRP'], labels=fix_atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2,explode=explode)
     nama_file = secrets.token_hex(nbytes=16) + ".png"
     plt.savefig("../public_html/cdc/data_olah/" + nama_file)
     if (fitur[3] != None):
@@ -205,13 +230,16 @@ def prodi_piechart(id_fitur):
     cur.execute("SELECT * FROM variabel WHERE id_fitur=%s", (fitur[0]))
 
     variabel = cur.fetchall()
-    i = 0
+    i = 1
     for a in variabel:
         atribut.append(a[1])
         explode.append(0.05)
         index.append(i)
         i+=1
-    excel="../public_html/cdc/excel/kuesioner/"+kuesioner[4]
+
+    fix_atribut = atribut
+
+    excel="../public_html/cdc/excel/    kuesioner/"+kuesioner[4]
     df = pd.read_excel(excel)
     df_olah = df.dropna(subset=[fitur[1]], axis=0)
     df_olah = df_olah[['Program Studi', 'NRP', fitur[1]]].groupby(['Program Studi', fitur[1]]).count()
@@ -222,34 +250,32 @@ def prodi_piechart(id_fitur):
     df_olah = df_olah.reset_index()
     # print(df_olah.info())
     # print(atribut)
-    # print(index)
-    # print(df_olah[fitur[1]].values)
+    print(index)
+    print(df_olah[fitur[1]].values)
     if (df_olah[fitur[1]].count() != 0):
         if (df_olah[fitur[1]].count() < len(atribut)):
             cek = array_diff(index,df_olah[fitur[1]].values)
             # print(cek)
             if cek is not None:
                 for j in cek:
-                    n = index.index(j)
-                    index.pop(n)
-                    atribut.pop(n)
-                    explode.pop(n)
+                    fix_atribut.remove(atribut[j - 1])
+                    explode.pop(0)
     else:
         new_row = {'Program Studi': prodi, fitur[1]: "NULL", 'NRP': 0}
         df_olah = df_olah.append(new_row, ignore_index=True)
-        atribut = ['Kosong']
+        fix_atribut = ['Kosong']
         explode = [0]
             # new_row = {'Program Studi': prodi, fitur[1]: "NULL", 'NRP': 0}
             # df_olah = df_olah.append(new_row, ignore_index=True)
 
-    atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in atribut]
+    fix_atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in atribut]
 
     # print(df_olah)
     # print(atribut)
     #code here
     plt.figure(figsize=(12, 6))
     plt.title("Persentase Mahasiswa "+prodi+" "+fitur[2], pad=20)
-    plt.pie(x=df_olah['NRP'], labels=atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2, explode=explode)
+    plt.pie(x=df_olah['NRP'], labels=fix_atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2, explode=explode)
     nama_file = secrets.token_hex(nbytes=16)+".png"
     plt.savefig("../public_html/cdc/data_olah/"+nama_file)
 
@@ -284,13 +310,16 @@ def prodi_piechart_non(id_fitur):
     cur.execute("SELECT * FROM variabel WHERE id_fitur=%s", (fitur[0]))
 
     variabel = cur.fetchall()
-    i = 0
+    i = 1
     for a in variabel:
         atribut.append(a[1])
         explode.append(0.05)
         index.append(i)
         i += 1
-    excel = "../public_html/cdc/excel/kuesioner/" + kuesioner[4]
+
+    fix_atribut = atribut
+
+    excel = "../public_html/cdc/excel/    kuesioner/" + kuesioner[4]
     df = pd.read_excel(excel)
     df_olah = df.dropna(subset=[fitur[1]], axis=0)
     df_olah = df_olah[['Program Studi', 'NRP', fitur[1]]].groupby(['Program Studi', fitur[1]]).count()
@@ -301,34 +330,33 @@ def prodi_piechart_non(id_fitur):
     df_olah = df_olah.reset_index()
     # print(df_olah.info())
     # print(atribut)
-    # print(index)
-    # print(df_olah[fitur[1]].values)
+    print(index)
+    print(df_olah[fitur[1]].values)
     if (df_olah[fitur[1]].count() != 0):
         if (df_olah[fitur[1]].count() < len(atribut)):
             cek = array_diff(index, df_olah[fitur[1]].values)
             # print(cek)
             if cek is not None:
                 for j in cek:
-                    n = index.index(j)
-                    index.pop(n)
-                    atribut.pop(n)
-                    explode.pop(n)
+                    fix_atribut.remove(atribut[j - 1])
+                    explode.pop(0)
     else:
         new_row = {'Program Studi': prodi, fitur[1]: "NULL", 'NRP': 0}
         df_olah = df_olah.append(new_row, ignore_index=True)
-        atribut = ['Kosong']
+        fix_atribut = ['Kosong']
         explode = [0]
         # new_row = {'Program Studi': prodi, fitur[1]: "NULL", 'NRP': 0}
         # df_olah = df_olah.append(new_row, ignore_index=True)
 
-    atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in atribut]
+    fix_atribut = [textwrap.TextWrapper(width=40).fill(text=l) for l in atribut]
 
     # print(df_olah)
     # print(atribut)
     # code here
     plt.figure(figsize=(12, 6))
     plt.title("Persentase Mahasiswa " + prodi + " " + fitur[2], pad=20)
-    plt.pie(x=df_olah['NRP'], labels=atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2, explode=explode)
+    plt.pie(x=df_olah['NRP'], labels=fix_atribut, autopct=make_autopct(df_olah['NRP']), labeldistance=1.2,
+            explode=explode)
     nama_file = secrets.token_hex(nbytes=16) + ".png"
     plt.savefig("../public_html/cdc/data_olah/" + nama_file)
 
